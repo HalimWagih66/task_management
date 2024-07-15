eb20ef22c29be1b9143ab4621a99158118d6a741
Abd Elhalim Wagih <halemwagih@gmail.com>
2024-07-15 12:41:52 +0300

    initialize the application

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TaskManagement extends StatelessWidget {
+  const TaskManagement({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body:Center(child: Text("welcome to app task management"),),
+      ),
+    );
+  }
+}
