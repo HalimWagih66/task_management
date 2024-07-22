84219fef6ab6a7badd926d9e3cd6e2769531d34e
Halim Wagih <77544016+HalimWagih66@users.noreply.github.com>
2024-07-22 14:57:34 +0300

    Revert "Feature/sign up with email"

--- lib/core/base/base_navigator.dart
+++ lib/core/base/base_navigator.dart
@@ -1,11 +1,10 @@
 import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:flutter/material.dart';
 
 abstract class BaseNavigator {
   void navigatePushScreenWithNamed(String name);
   void showLoading();
   void navigatePopFromThisScreen();
-  Future<void> showDialogForUploadImage();
-  void displayMessageWithSnackPar(String message);
   void displayMessageWithAwesomeDialog({required String message, required DialogType dialogType, String? title, String? posActionName, Function? posAction, bool dismissOnTouchOutside = false, String? nigActionName, Function? nigAction});
-  void pushScreenReplacementWithNamed(String routeName);
+
 }
--- lib/features/auth/choose the registration method screen/presentation/view/choose_the_registration_method_screen.dart
+++ lib/features/auth/choose the registration method screen/presentation/view/choose_the_registration_method_screen.dart
@@ -31,5 +31,6 @@ class _ChooseTheRegistrationMethodScreenState extends BaseState<ChooseTheRegistr
   }
 
 
+  
 
 }
